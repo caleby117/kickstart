d66176e900aa90daee050cb5387dbb6a360c9b50
Caleb Yoong <caleb.yoong216@gmail.com>
2024-11-13 16:52:08 +0800

    feat(config) highlight 80th col

--- init.lua
+++ init.lua
@@ -154,6 +154,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Highlight the 80th column
+vim.opt.colorcolumn = { 80 }
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
